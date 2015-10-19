c715a1b3c1ceb9dc2f745eb65e014912eafdf7eb
Stéphane Albert <stephane.albert@objectif-libre.com>
2015-10-19 12:21:12 +0200

    Fixed enabled files for Liberty release
    We used to have only one enabled file and tricked horizon in loading all
    others enabled files as if it was a dashboard. This trick is not working
    anymore.
    
    Closes-Bug: #1507087
    Change-Id: I7b075ab66d623e28efb174191ad0ebd359ec06a2

--- cloudkittydashboard/enabled/_11_admin_hashmap_panel.py
+++ cloudkittydashboard/enabled/_11_admin_hashmap_panel.py
@@ -12,6 +12,8 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+ADD_INSTALLED_APPS = ['cloudkittydashboard']
+
 PANEL_GROUP = 'rating'
 PANEL_DASHBOARD = 'admin'
 PANEL = 'hashmap'
