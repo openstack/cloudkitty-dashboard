6c76a41a582e2bf1b752e90aa8cb21972ecec9da
Leonie Chamberlin-Medd <leonie@stackhpc.com>
2025-08-13 14:56:56 +0000

    Adds datepicker and improved graph/piechart
    Adds ability to control time period shown on graph and pie chart in
    reporting tab. Preset range buttons have been included under two
    dropdown buttons, and it is possible to see the previous/next set range
    with the arrow buttons.
    
    Interactive legends have been added to both the datepicker and the
    piechart.
    
    Change-Id: Ieea8f22a5ac7e21996d4bd4223ea01694591bd72
    Signed-off-by: Leonie Chamberlin-Medd <leonie@stackhpc.com>

--- cloudkittydashboard/dashboards/project/reporting/views.py
+++ cloudkittydashboard/dashboards/project/reporting/views.py
@@ -18,9 +18,15 @@ import datetime
 import decimal
 import time
 
+from horizon import messages
 from horizon import tabs
 
+from django.conf import settings
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
 from cloudkittydashboard.api import cloudkitty as api
+from cloudkittydashboard import forms
 
 
 def _do_this_month(data):
@@ -33,9 +39,12 @@ def _do_this_month(data):
     end_timestamp = None
     for dataframe in data.get('dataframes', []):
         begin = dataframe['begin']
-        timestamp = int(time.mktime(
-            datetime.datetime.strptime(begin[:16],
-                                       "%Y-%m-%dT%H:%M").timetuple()))
+        timestamp = int(
+            time.mktime(
+                datetime.datetime.strptime(
+                    begin[:16], "%Y-%m-%dT%H:%M").timetuple()
+            )
+        )
         if start_timestamp is None or timestamp < start_timestamp:
             start_timestamp = timestamp
         if end_timestamp is None or timestamp > end_timestamp:
@@ -69,25 +78,102 @@ def _do_this_month(data):
 
 
 class CostRepartitionTab(tabs.Tab):
-    name = "This month"
+    name = _("This month")
     slug = "this_month"
     template_name = 'project/reporting/this_month.html'
 
     def get_context_data(self, request, **kwargs):
         today = datetime.datetime.today()
         day_start, day_end = calendar.monthrange(today.year, today.month)
-        begin = "%4d-%02d-01T00:00:00" % (today.year, today.month)
-        end = "%4d-%02d-%02dT23:59:59" % (today.year, today.month, day_end)
+
+        form = self.get_form()
+
+        if form.is_valid():
+            # set values to be from datepicker form
+            start = form.cleaned_data['start']
+            end = form.cleaned_data['end']
+            begin = "%4d-%02d-%02dT00:00:00" % (start.year,
+                                                start.month, start.day)
+            end = "%4d-%02d-%02dT23:59:59" % (end.year, end.month, end.day)
+
+            if end < begin:
+                messages.error(
+                    self.request,
+                    _("Invalid time period. The end date should be "
+                        "more recent than the start date."
+                        " Setting the end as today."))
+
+                end = "%4d-%02d-%02dT23:59:59" % (today.year,
+                                                  today.month, day_end)
+
+            elif start > today.date():
+                messages.error(
+                    self.request,
+                    _("Invalid time period. You are requesting "
+                      "data from the future which may not exist."))
+
+        elif form.is_bound:
+            messages.error(
+                self.request, _(
+                    "Invalid date format: Using this month as default.")
+                )
+
+            begin = "%4d-%02d-%02dT00:00:00" % (today.year,
+                                                today.month, day_start)
+            end = "%4d-%02d-%02dT23:59:59" % (today.year, today.month, day_end)
+
+        else:  # set default date values (before form is filled in)
+            begin = "%4d-%02d-%02dT00:00:00" % (today.year,
+                                                today.month, day_start)
+            end = "%4d-%02d-%02dT23:59:59" % (today.year, today.month, day_end)
+
         client = api.cloudkittyclient(request)
         data = client.storage.get_dataframes(
             begin=begin, end=end, tenant_id=request.user.tenant_id)
         parsed_data = _do_this_month(data)
-        return {'repartition_data': parsed_data}
+        return {'repartition_data': parsed_data, 'form': form}
+
+    @property
+    def today(self):
+        return timezone.now()
+
+    @property
+    def first_day(self):
+        days_range = settings.OVERVIEW_DAYS_RANGE
+        if days_range:
+            return self.today.date() - datetime.timedelta(days=days_range)
+        return datetime.date(self.today.year, self.today.month, 1)
+
+    def init_form(self):
+        self.start = self.first_day
+        self.end = self.today.date()
+
+        return self.start, self.end
+
+    def get_form(self):
+        if not hasattr(self, "form"):
+            req = self.request
+            start = req.GET.get('start', req.session.get('usage_start'))
+            end = req.GET.get('end', req.session.get('usage_end'))
+            if start and end:
+                # bound form
+                self.form = forms.DateForm({'start': start, 'end': end})
+
+            else:
+                # non-bound form
+                init = self.init_form()
+                start = init[0].isoformat()
+                end = init[1].isoformat()
+                self.form = forms.DateForm(
+                    initial={'start': start, 'end': end})
+            req.session['usage_start'] = start
+            req.session['usage_end'] = end
+        return self.form
 
 
 class ReportingTabs(tabs.TabGroup):
     slug = "reporting_tabs"
-    tabs = (CostRepartitionTab, )
+    tabs = (CostRepartitionTab,)
     sticky = True
 
 
